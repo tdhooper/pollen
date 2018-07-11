abf95bd60022efc0954a0cc485d842826e2551e8
Thomas Hooper <tom@stainlessvision.com>
2018-07-12 00:27:38 +0100

    Deform mesh height with video brightness

--- js/index.js
+++ js/index.js
@@ -12,20 +12,61 @@ camera.distance = 10;
 window.addEventListener('resize', fit(canvas), false);
 
 var mesh;
-mesh = geometry.icosahedron(2);
-// mesh = geometry.tetrahedron(3);
+mesh = geometry.icosahedron(4);
+// mesh = geometry.tetrahedron(5);
 
 var webcam = new WebcamTexture(regl);
+
+
+var heightMap = regl.framebuffer({
+  depth: false,
+  width: 1024,
+  height: 1024
+});
+
+const drawHeightMap = regl({
+  frag: `
+    precision mediump float;
+    uniform sampler2D video;
+    varying vec2 vuv;
+    void main () {
+      vec3 tex = texture2D(video, vuv).rgb;
+      float height = length(tex);
+      gl_FragColor = vec4(vec3(height), 1);
+    }`,
+  vert: `
+    precision mediump float;
+    attribute vec2 position;
+    varying vec2 vuv;
+    void main () {
+      vuv = position;
+      gl_Position = vec4(2. * position - 1., 0, 1);
+    }`,
+  attributes: {
+    position: [
+      -2, 0,
+      0, -2,
+      2, 2
+    ],
+  },
+  count: 3,
+  uniforms: {
+    video: webcam.texture
+  },
+  framebuffer: heightMap
+});
 
 const drawSphere = regl({
   frag: `
     precision mediump float;
     varying vec3 vnormal;
     varying vec2 vuv;
+    varying float height;
     uniform sampler2D video;
     void main () {
         vec3 tex = texture2D(video, vec2(1) - vuv).rgb;
         gl_FragColor = vec4(tex, 1);
+        // gl_FragColor = vec4(vec3(height), 1);
         // gl_FragColor = vec4(vnormal * .5 + .5, 1.0);
     }`,
   vert: `
@@ -33,15 +74,20 @@ const drawSphere = regl({
     uniform mat4 proj;
     uniform mat4 model;
     uniform mat4 view;
+    uniform sampler2D heightMap;
     attribute vec3 position;
     attribute vec3 normal;
     attribute vec2 uv;
     varying vec3 vnormal;
     varying vec2 vuv;
+    varying float height;
     void main () {
       vnormal = normal;
       vuv = uv;
-      gl_Position = proj * view * model * vec4(position, 1.0);
+      height = texture2D(heightMap, vec2(1) - vuv).r;
+      vec3 pos = position;
+      pos *= mix(.5, 1., height);
+      gl_Position = proj * view * model * vec4(pos, 1.0);
     }`,
   attributes: {
     position: mesh.positions,
@@ -58,7 +104,8 @@ const drawSphere = regl({
         1000),
     model: mat4.identity([]),
     view: () => camera.view(),
-    video: webcam.texture
+    video: webcam.texture,
+    heightMap: heightMap
   }
 })
 
@@ -69,5 +116,6 @@ regl.frame(() => {
   camera.rotate([.003,0.002],[0,0]);
   camera.tick()
   webcam.update();
+  drawHeightMap();
   drawSphere()
 })
