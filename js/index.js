c4c7b2e4ca21d3be04eef473d170187ec4226a39
Thomas Hooper <tom@stainlessvision.com>
2018-07-12 19:39:19 +0100

    Use glslify to import blur

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const mat4 = require('gl-mat4');
 const fit = require('canvas-fit');
 const normals = require('angle-normals');
+const glslify = require('glslify');
 const geometry = require('./geometry/polyhedra');
 const WebcamTexture = require('./webcam-texture');
 
@@ -78,31 +79,18 @@ const resamplePass = regl({
 });
 
 const blurPass = regl({
-  frag: `
+  frag: glslify(`
     precision mediump float;
     uniform sampler2D source;
     uniform vec2 direction;
     uniform vec2 resolution;
 
-    vec4 blur13(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {
-      vec4 color = vec4(0.0);
-      vec2 off1 = vec2(1.411764705882353) * direction;
-      vec2 off2 = vec2(3.2941176470588234) * direction;
-      vec2 off3 = vec2(5.176470588235294) * direction;
-      color += texture2D(image, uv) * 0.1964825501511404;
-      color += texture2D(image, uv + (off1 / resolution)) * 0.2969069646728344;
-      color += texture2D(image, uv - (off1 / resolution)) * 0.2969069646728344;
-      color += texture2D(image, uv + (off2 / resolution)) * 0.09447039785044732;
-      color += texture2D(image, uv - (off2 / resolution)) * 0.09447039785044732;
-      color += texture2D(image, uv + (off3 / resolution)) * 0.010381362401148057;
-      color += texture2D(image, uv - (off3 / resolution)) * 0.010381362401148057;
-      return color;
-    }
+    #pragma glslify: blur = require('glsl-fast-gaussian-blur/13')
 
     void main() {
       vec2 uv = vec2(gl_FragCoord.xy / resolution.xy);
-      gl_FragColor = blur13(source, uv, resolution.xy, direction);
-    }`,
+      gl_FragColor = blur(source, uv, resolution.xy, direction);
+    }`),
   uniforms: {
     direction: regl.prop('direction'),
     source: regl.prop('source'),
