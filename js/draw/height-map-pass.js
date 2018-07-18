9d23c142bc38d8d1e05577ec91890f2ce591b5c4
Thomas Hooper <tom@stainlessvision.com>
2018-07-18 20:05:41 +0100

    Colour difference height map

--- js/draw/height-map-pass.js
+++ js/draw/height-map-pass.js
@@ -2,18 +2,25 @@
 const heightMapPass = regl({
   frag: `
     precision mediump float;
+    uniform sampler2D difference;
     uniform sampler2D source;
     uniform vec2 resolution;
 
     void main () {
       vec2 uv = vec2(gl_FragCoord.xy / resolution.xy);
       vec3 tex = texture2D(source, uv).rgb;
-      float height = length(tex);
+      vec3 a = texture2D(difference, vec2(0, 0)).rgb;
+      vec3 b = texture2D(difference, vec2(1, 0)).rgb;
+      float aDist = distance(tex, a);
+      float bDist = distance(tex, b);
+      float height = aDist / (aDist + bDist);
       height = pow(height, 2.);
       gl_FragColor = vec4(vec3(height), 1);
       // gl_FragColor = vec4(tex, 1);
+      // gl_FragColor = vec4(b, 1);
     }`,
   uniforms: {
+    difference: regl.prop('difference'),
     source: regl.prop('source'),
     resolution: function(context) {
       return [context.framebufferWidth, context.framebufferHeight];
--- js/index.js
+++ js/index.js
@@ -72,8 +72,51 @@ const blurBuffers = [0,0].map(function() {
   });
 });
 
+var diffSize = 6;
+
+const diffSourceBuffer = regl.framebuffer({
+  depth: false,
+  color: regl.texture({
+    width: diffSize,
+    height: diffSize
+  })
+});
+
+const diffBuffer = regl.framebuffer({
+  depth: false,
+  color: regl.texture({
+    width: diffSize * diffSize,
+    height: diffSize * diffSize
+  })
+});
+
+const diffReduceABuffer = regl.framebuffer({
+  depth: false,
+  color: regl.texture({
+    width: diffSize * diffSize,
+    height: 1
+  })
+});
+
+const diffReduceBBuffer = regl.framebuffer({
+  depth: false,
+  color: regl.texture({
+    width: 1,
+    height: 1
+  })
+});
+
+const diffResultBuffer = regl.framebuffer({
+  depth: false,
+  color: regl.texture({
+    width: 2,
+    height: 1
+  })
+});
+
+
 var videoMat = glm.mat3.create();
-var videoScale = -2;
+var videoScale = -4;
 var videoTranslate = .5 / videoScale - .5;
 glm.mat3.scale(videoMat, videoMat, [videoScale, videoScale]);
 glm.mat3.translate(videoMat, videoMat, [videoTranslate, videoTranslate]);
@@ -86,12 +129,114 @@ glm.mat3.invert(previewMat, previewMat);
 
 var previewMatViewport = glm.mat3.create();
 
+
+
+const differencesPass = regl({
+  frag: `
+    precision mediump float;
+    uniform sampler2D source;
+    uniform vec2 sourceSize;
+    uniform vec2 direction;
+
+    void main() {
+      vec2 ab = gl_FragCoord.xy;
+      vec2 uvA = vec2(
+        mod(ab.x, sourceSize.x),
+        floor(ab.x / sourceSize.x)
+      );
+      vec2 uvB = vec2(
+        mod(ab.y, sourceSize.x),
+        floor(ab.y / sourceSize.x)
+      );
+      vec3 a = texture2D(source, uvA / sourceSize).rgb;
+      vec3 b = texture2D(source, uvB / sourceSize).rgb;
+      float difference = distance(a, b) / 2.;
+      gl_FragColor = vec4(difference, ab / 255., 1);
+    }`,
+  uniforms: {
+    source: regl.prop('source'),
+    sourceSize: function(context, props) {
+      return [props.source.width, props.source.height];
+    }
+  },
+  framebuffer: regl.prop('destination')
+});
+
+const maxDifferencesPass = regl({
+  frag: `
+    precision mediump float;
+    uniform sampler2D source;
+    uniform vec2 sourceSize;
+
+    void main() {
+      vec2 uv;
+      vec3 maxData = vec3(0);
+      for (float i = 0.; i < ${ diffSize * diffSize }.; i++) {
+        uv = vec2(gl_FragCoord.x, i);
+        vec3 data = texture2D(source, uv / sourceSize).rgb;
+        if (data.r > maxData.r) {
+          maxData = data;
+        }
+      }
+      gl_FragColor = vec4(maxData, 1);
+    }`,
+  uniforms: {
+    direction: regl.prop('direction'),
+    source: regl.prop('source'),
+    sourceSize: function(context, props) {
+      return [props.source.width, props.source.height];
+    }
+  },
+  framebuffer: regl.prop('destination')
+});
+
+const resultPass = regl({
+  frag: `
+    precision mediump float;
+    uniform sampler2D result;
+    uniform sampler2D source;
+    uniform vec2 sourceSize;
+    uniform vec2 resolution;
+
+    void main() {
+      vec2 uv = gl_FragCoord.xy / resolution;
+      vec2 ab = texture2D(result, vec2(0)).gb;
+      ab *= 255.;
+      vec2 uvA = vec2(
+        mod(ab.x, sourceSize.x),
+        floor(ab.x / sourceSize.x)
+      );
+      vec2 uvB = vec2(
+        mod(ab.y, sourceSize.x),
+        floor(ab.y / sourceSize.x)
+      );
+      if (uv.x > .5) {
+        gl_FragColor = texture2D(source, uvA / sourceSize);
+      } else {
+        gl_FragColor = texture2D(source, uvB / sourceSize);
+      }
+    }`,
+  uniforms: {
+    direction: regl.prop('direction'),
+    result: regl.prop('result'),
+    source: regl.prop('source'),
+    sourceSize: function(context, props) {
+      return [props.source.width, props.source.height];
+    },
+    resolution: function(context) {
+      return [context.framebufferWidth, context.framebufferHeight];
+    }
+  },
+  framebuffer: regl.prop('destination')
+});
+
+
 regl.frame((context) => {
   regl.clear({
     color: [.8, .82, .85, 1]
   })
   camera.rotate([.003,0.002],[0,0]);
-  camera.tick()
+  camera.tick();
   webcam.update();
   setupPass(function() {
     resamplePass({
@@ -99,6 +244,8 @@ regl.frame((context) => {
       destination: croppedVideo,
       transform: videoMat
     });
+
+
     resamplePass({
       source: croppedVideo,
       destination: blurBuffers[0]
@@ -117,10 +264,38 @@ regl.frame((context) => {
       });
       i += 1;
     }
+
+    resamplePass({
+      source: blurBuffers[0],
+      destination: diffSourceBuffer
+    });
+    differencesPass({
+      source: diffSourceBuffer,
+      destination: diffBuffer
+    });
+    maxDifferencesPass({
+      source: diffBuffer,
+      destination: diffReduceABuffer
+    });
+    maxDifferencesPass({
+      source: diffReduceABuffer,
+      destination: diffReduceBBuffer
+    });
+    resultPass({
+      result: diffReduceBBuffer,
+      source: diffSourceBuffer,
+      destination: diffResultBuffer
+    });
+
+
     heightMapPass({
+      difference: diffResultBuffer,
       source: blurBuffers[0],
       destination: blurBuffers[1]
     });
+    // resamplePass({
+    //   source: blurBuffers[1]
+    // });
   });
   drawSphere({
     heightMap: blurBuffers[1],
@@ -128,7 +303,6 @@ regl.frame((context) => {
     view: camera.view(),
     mesh: mesh
   });
-  // console.log(context);
   var ratio = context.drawingBufferWidth / context.drawingBufferHeight;
   glm.mat3.scale(previewMatViewport, previewMat, [ratio, 1]);
   setupPass(function() {
