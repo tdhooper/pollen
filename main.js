cc170f8d0062e0c80ade7b127a97a0ec30a9a268
Thomas Hooper <tom@stainlessvision.com>
2018-10-17 22:37:41 +0100

    Save locally with hash for filename

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ var url = require('url');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var empty = require('is-empty');
+var crypto = require('crypto');
+var path = require('path');
+
+var saveLocation = path.join(__dirname, 'saved');
+
+if ( ! fs.existsSync(saveLocation)){
+  fs.mkdirSync(saveLocation);
+}
 
 budo('./js/index.js', {
   live: true,
@@ -16,12 +24,14 @@ budo('./js/index.js', {
           res.statusCode = 500;
           res.end('Missing content');
         }
-        content = JSON.stringify(req.body, null, 4);
+        var content = JSON.stringify(req.body, null, 4);
         content += '\n';
-        fs.writeFile("/tmp/test", content, function(err) {
+        var filename = crypto.createHash('md5').update(content).digest("hex");
+        filename += '.json';
+        var file = path.join(saveLocation, filename);
+        fs.writeFile(file, content, function(err) {
           if (err) {
-            res.statusCode = 500;
-            res.end(err);
+            throw err;
           }
           res.statusCode = 200;
           res.end('Saved');
