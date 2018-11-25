4236822b64e7e8c4efc3d1330a10283f5d8999b8
Thomas Hooper <tom@stainlessvision.com>
2018-11-25 23:40:17 +0000

    FMP with less visible movement

--- js/process/back-blur.js
+++ js/process/back-blur.js
@@ -28,35 +28,44 @@ class BackBlur {
 //----------------------------------------------------------------------------------------
 //  1 out, 1 in...
 
-float hash(float n) { return fract(sin(n) * 1e4); }
-float hash(vec2 p) { return fract(1e4 * sin(17.0 * p.x + p.y * 0.1) * (0.1 + abs(sin(p.y * 13.0 + p.x)))); }
+float hash(float p)
+{
+    vec3 p3  = fract(vec3(p) * HASHSCALE1);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return fract((p3.x + p3.y) * p3.z);
+}
 
 
 // Based on Morgan McGuire @morgan3d
 // https://www.shadertoy.com/view/4dS3Wd
-float noise(vec2 x) {
-  vec2 i = floor(x);
-  vec2 f = fract(x);
-  float a = hash(i);
-  float b = hash(i + vec2(1.0, 0.0));
-  float c = hash(i + vec2(0.0, 1.0));
-  float d = hash(i + vec2(1.0, 1.0));
-  vec2 u = f * f * (3.0 - 2.0 * f);
-  return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
-}
+float noise(vec3 x) {
+    const vec3 step = vec3(110, 241, 171);
 
+    vec3 i = floor(x);
+    vec3 f = fract(x);
+ 
+    // For performance, compute the base input to a 1D hash from the integer part of the argument and the 
+    // incremental change to the 1D based on the 3D -> 1D wrapping
+    float n = dot(i, step);
+
+    vec3 u = f * f * (3.0 - 2.0 * f);
+    return mix(mix(mix( hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y),
+               mix(mix( hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z);
+}
 
 #define NUM_OCTAVES 2
 
-float fbm ( in vec2 _st) {
+float fbm ( in vec3 _st) {
     float v = 0.0;
     float a = 0.5;
-    vec2 shift = vec2(20);
+    vec3 shift = vec3(20.0);
     // Rotate to reduce axial bias
-    mat2 rot = mat2(
-      cos(.5), sin(.5),
-      -sin(.5), cos(.5)
-    );
+    mat3 rot = mat3(cos(0.5), sin(0.5), 0,
+                    -sin(0.5), cos(0.50), 0,
+                    0, 0, 1
+                );
     for (int i = 0; i < NUM_OCTAVES; ++i) {
         v += a * noise(_st);
         _st = rot * _st * 2.2 + shift;
@@ -65,15 +74,15 @@ float fbm ( in vec2 _st) {
     return v;
 }
 
-vec2 map(float time, vec2 st) {
+vec2 map(vec3 st) {
 
     vec2 a, b;
     
     a.x = fbm(st);
-    a.y = fbm(st + vec2(1));
+    a.y = fbm(st + vec3(1));
 
-    b.x = fbm(st + 4. * a + time * .05);
-    b.y = fbm(st + time * .2);
+    b.x = fbm(st + 4. * vec3(a,0));
+    b.y = fbm(st + 2. * vec3(a,0));
 
     return normalize(b - .4);
 }
@@ -82,7 +91,7 @@ vec2 map(float time, vec2 st) {
 
         void main() {
           vec2 xy = gl_FragCoord.xy;
-          xy += map(time * 10., xy * .2) * 2.5;
+          xy += map(vec3(xy * .2, time * 2.)) * 2.5;
           vec2 uv = vec2(xy / resolution.xy);
           gl_FragColor = texture2D(source, uv) * vec4(vec3(1.01), 1);
         }`,
