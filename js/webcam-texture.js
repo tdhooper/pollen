bfb80006af95b242b75c7a4d9d198c80bf27b8ac
Thomas Hooper <tom@stainlessvision.com>
2018-11-29 00:04:27 +0000

    Force microscope, and reaquire when lost

--- js/webcam-texture.js
+++ js/webcam-texture.js
@@ -12,25 +12,53 @@ var WebcamTexture = function(regl) {
 
     this.texture = regl.texture();
 
+    navigator.mediaDevices.enumerateDevices().then(function(e) {
+        console.log(e);
+    });
+
+    this.startStream();
+};
+
+WebcamTexture.prototype.startStream = function() {
+    console.log('Starting stream...');
     var constraints = {
         video: {
-            facingMode: 'environment'
+            deviceId: {exact: '60f187ac2825165f0d813c0a7743e8691ad84a5775427c7146ad2239a3a900b2'},
         }
     };
     navigator.mediaDevices.getUserMedia(constraints)
-        .then(this.gotStream.bind(this))
-        .catch(function(err) {
+        .then(this.getVideoFromStream.bind(this))
+        .then(this.texture)
+        .catch(err => { 
             console.log(err.name + ": " + err.message);
+            console.log('Restarting in 1 second');
+            setTimeout(this.startStream.bind(this), 1000);
         });
 };
 
-WebcamTexture.prototype.gotStream = function(stream) {
+WebcamTexture.prototype.getVideoFromStream = function(stream) {
+    console.log('Got stream');
+    var track = stream.getVideoTracks()[0];
+    track.onended = this.streamEnded.bind(this);
     this.video.srcObject = stream;
-    this.video.onloadedmetadata = function(e) {
-        this.video.play();
-        this.texture(this.video);
-        this.ready = true;
-    }.bind(this);
+    return new Promise((resolve, reject) => {
+        this.video.onloadedmetadata = function(e) {
+            if ( ! this.video.videoHeight || ! this.video.videoWidth) {
+                reject(new Error('Invalid video size'));
+            } else {
+                console.log('Got video');
+                this.ready = true;
+                resolve(this.video);
+            }
+        }.bind(this);
+    });
+};
+
+WebcamTexture.prototype.streamEnded = function() {
+    console.log('Stream ended');
+    this.ready = false;
+    console.log('Restarting in 1 second');
+    setTimeout(this.startStream.bind(this), 1000);
 };
 
 WebcamTexture.prototype.update = function() {
